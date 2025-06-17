04e2ca3d49940e7326b9448ef35cda2ab2fef484
Dealer63636j <dealersausage@gmail.com>
2025-06-17 15:33:31 +0530

    pptx

--- pptx/9.php
+++ pptx/9.php
@@ -1,12 +1,8 @@
 <?php
-$a = 2;
-$b = 3;
-$ans = 0;
-function multiplication()
-{
-    global $a, $b, $ans;
-    $ans = $a * $b;
-}
-multiplication();
-echo $ans;
+    function greet() { 
+       $msg = "hello";
+       echo $msg; 
+     }
+
+     greet();
 ?>
